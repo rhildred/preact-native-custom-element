9b054754bae444de29ce54ad68998998056a07de
Rich Hildred <rhildred@gmail.com>
2026-03-13 21:39:56 +0000

    add chat component from https://github.com/keith-kurak/ChatUIExample

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./index.css";
 import { ItWorks } from "./components/ItWorks.jsx";
 import register from 'preact-custom-element';
+import ChatView from "./components/ChatView.jsx";
 
 register(ItWorks, "x-it-works");
+register(ChatView, "x-chat");
